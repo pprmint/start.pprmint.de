164a0be74f50ce5e701d5cc402328350b0ff8daf
Nicklas <git@pprmint.de>
2024-07-30 07:04:30 +0000

    Figured it might make sense to categorize clocks as I come up with more ideas.

--- src/components/clock.tsx
+++ src/components/clock.tsx
@@ -5,19 +5,27 @@ import DigitalClockNoAnim from "./clocks/digitalNoAnim";
 import SwissClock from "./clocks/analogSwiss";
 import BahnClock from "./clocks/analogGerman";
 
+export const Clocks = [
+  { name: "Digital", clocks: ["Digital", "Digital (no animations)"] },
+  {
+    name: "Analog",
+    clocks: ["Swiss station clock", "German station clock"],
+  },
+];
+
 function Clock() {
-	const lsClock = localStorage.getItem("clock");
-	return lsClock === "German station clock" ? (
-		<BahnClock />
-	) : lsClock === "Swiss station clock" ? (
-		<SwissClock />
-	) : lsClock === "Digital (no animations)" ? (
-		<DigitalClockNoAnim />
-	) : lsClock === "None" ? null : (
-		<DigitalClock />
-	);
+  const lsClock = localStorage.getItem("clock");
+  return lsClock === "German station clock" ? (
+    <BahnClock />
+  ) : lsClock === "Swiss station clock" ? (
+    <SwissClock />
+  ) : lsClock === "Digital (no animations)" ? (
+    <DigitalClockNoAnim />
+  ) : lsClock === "None" ? null : (
+    <DigitalClock />
+  );
 }
 
 export default dynamic(() => Promise.resolve(Clock), {
-	ssr: false,
+  ssr: false,
 });
