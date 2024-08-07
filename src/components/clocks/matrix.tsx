43f85587511382e9805f62e9f8136d10206bf569
Nicklas <git@pprmint.de>
2024-08-07 06:42:20 +0000

    Refactor to get rid of hundreds of divs as a background.

--- src/components/clocks/matrix.tsx
+++ src/components/clocks/matrix.tsx
@@ -2,32 +2,58 @@
 import { useMemo } from "react";
 
 const Patterns = {
-	0: [0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0],
-	1: [1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1],
-	2: [1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1],
-	3: [1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0],
-	4: [0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0],
-	5: [1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0],
-	6: [0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0],
-	7: [1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0],
-	8: [0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0],
-	9: [0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 1, 1, 0],
+	0: [
+		0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0,
+		0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0,
+	],
+	1: [
+		0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1,
+		0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0,
+	],
+	2: [
+		0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0,
+		0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0,
+	],
+	3: [
+		0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0,
+		0, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0,
+	],
+	4: [
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0,
+		1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,
+	],
+	5: [
+		0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0,
+		0, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0,
+	],
+	6: [
+		0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0,
+		0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0,
+	],
+	7: [
+		0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1,
+		0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	],
+	8: [
+		0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0,
+		0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0,
+	],
+	9: [
+		0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0,
+		0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0,
+	],
 };
 
 function Display({ digit }: { digit?: keyof typeof Patterns }) {
-	const Colon = [0, 0, 1, 0, 1, 0, 0];
+	const Colon = [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0];
 	return (
-		<div
-			className={`grid ${
-				digit !== undefined && digit !== null ? "grid-cols-5 w-[68px]" : "grid-cols-1 w-3"
-			} gap-0.5 h-24 `}
-		>
+		<div className={`grid ${digit !== undefined && digit !== null ? "grid-cols-6" : "grid-cols-2"} gap-0.5 `}>
 			{digit !== undefined && digit !== null
 				? Patterns[digit].map((dot, index) => (
 						<div
 							key={index}
-							className={`bg-foreground-2 ${
-								dot === 1 ? "opacity-100 size-3" : "opacity-0 size-3"
+							className={`size-3 ${
+								dot === 1 ? "bg-foreground-2" : "bg-elevate-1/50 shadow-inner"
 							} rounded-sm duration-200 ease-out`}
 							style={{ transitionDelay: `${index / 150}s` }}
 						/>
@@ -35,7 +61,9 @@ function Display({ digit }: { digit?: keyof typeof Patterns }) {
 				: Colon.map((dot, index) => (
 						<div
 							key={index}
-							className={`${dot === 1 && "bg-accent"} size-3 rounded-sm duration-200 ease-out`}
+							className={`${
+								dot === 1 ? "bg-accent" : "bg-elevate-1/50 shadow-inner"
+							} size-3 rounded-sm duration-200 ease-out`}
 							style={{ transitionDelay: `${index / 150}s` }}
 						/>
 				  ))}
@@ -46,26 +74,24 @@ function Display({ digit }: { digit?: keyof typeof Patterns }) {
 export default function MatrixClock({ time, meridiem }: { time: string; meridiem?: "AM" | "PM" }) {
 	const digits = useMemo(() => time.split("").map(Number), [time]);
 	return (
-		<div className="relative p-1 border border-elevate-2 rounded-md bg-gradient-to-b from-elevate-2/25">
-			<div className="absolute -z-10 inset-1 grid grid-rows-9 grid-flow-col gap-0.5">
-				{[...Array(digits.length > 4 ? 369 : 243)].map((_, index) => (
-					<div key={index} className="bg-elevate-1/50 size-3 rounded-sm shadow-inner" />
+		<div className="flex gap-0.5 p-1 border border-elevate-2 rounded-md bg-gradient-to-b from-elevate-2/25">
+			<div className="grid gap-0.5 grid-cols-1">
+				{[...Array(9)].map((_, index) => (
+					<div key={index} className="size-3 bg-elevate-1/50 shadow-inner rounded-sm" />
 				))}
 			</div>
-			<div className="flex gap-4 p-3.5 drop-shadow-[0px_1px_1px_#0003]">
-				<Display digit={digits[0] as keyof typeof Patterns} />
-				<Display digit={digits[1] as keyof typeof Patterns} />
-				<Display />
-				<Display digit={digits[2] as keyof typeof Patterns} />
-				<Display digit={digits[3] as keyof typeof Patterns} />
-				{digits.length > 4 && (
-					<>
-						<Display />
-						<Display digit={digits[4] as keyof typeof Patterns} />
-						<Display digit={digits[5] as keyof typeof Patterns} />
-					</>
-				)}
-			</div>
+			<Display digit={digits[0] as keyof typeof Patterns} />
+			<Display digit={digits[1] as keyof typeof Patterns} />
+			<Display />
+			<Display digit={digits[2] as keyof typeof Patterns} />
+			<Display digit={digits[3] as keyof typeof Patterns} />
+			{digits.length > 4 && (
+				<>
+					<Display />
+					<Display digit={digits[4] as keyof typeof Patterns} />
+					<Display digit={digits[5] as keyof typeof Patterns} />
+				</>
+			)}
 		</div>
 	);
 }
